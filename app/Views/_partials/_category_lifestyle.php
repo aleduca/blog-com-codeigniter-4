0f22c6e549c165f3bdf29565769f80e56838cb84
Alexandre Cardoso <xandecar@hotmail.com>
2022-11-22 19:20:46 -0300

    Load template with fetch API complete

---
+++ app/Views/_partials/_category_lifestyle.php
@@ -0,0 +1,116 @@
+<div class="container" data-aos="fade-up">
+  <div class="section-header d-flex justify-content-between align-items-center mb-5">
+    <h2>Lifestyle</h2>
+    <div><a href="category.html" class="more">See All Lifestyle</a></div>
+  </div>
+
+  <div class="row g-5">
+    <div class="col-lg-4">
+      <div class="post-entry-1 lg">
+        <a href="single-post.html"><img src="assets/img/post-landscape-8.jpg" alt="" class="img-fluid"></a>
+        <div class="post-meta"><span class="date">Lifestyle</span> <span class="mx-1">&bullet;</span> <span>Jul 5th '22</span></div>
+        <h2><a href="single-post.html">11 Work From Home Part-Time Jobs You Can Do Now</a></h2>
+        <p class="mb-4 d-block">Lorem ipsum dolor sit, amet consectetur adipisicing elit. Vero temporibus repudiandae, inventore pariatur numquam cumque possimus exercitationem? Nihil tempore odit ab minus eveniet praesentium, similique blanditiis molestiae ut saepe perspiciatis officia nemo, eos quae cumque. Accusamus fugiat architecto rerum animi atque eveniet, quo, praesentium dignissimos</p>
+
+        <div class="d-flex align-items-center author">
+          <div class="photo"><img src="assets/img/person-7.jpg" alt="" class="img-fluid"></div>
+          <div class="name">
+            <h3 class="m-0 p-0">Esther Howard</h3>
+          </div>
+        </div>
+      </div>
+
+      <div class="post-entry-1 border-bottom">
+        <div class="post-meta"><span class="date">Lifestyle</span> <span class="mx-1">&bullet;</span> <span>Jul 5th '22</span></div>
+        <h2 class="mb-2"><a href="single-post.html">The Best Homemade Masks for Face (keep the Pimples Away)</a></h2>
+        <span class="author mb-3 d-block">Jenny Wilson</span>
+      </div>
+
+      <div class="post-entry-1">
+        <div class="post-meta"><span class="date">Lifestyle</span> <span class="mx-1">&bullet;</span> <span>Jul 5th '22</span></div>
+        <h2 class="mb-2"><a href="single-post.html">10 Life-Changing Hacks Every Working Mom Should Know</a></h2>
+        <span class="author mb-3 d-block">Jenny Wilson</span>
+      </div>
+
+    </div>
+
+    <div class="col-lg-8">
+      <div class="row g-5">
+        <div class="col-lg-4 border-start custom-border">
+          <div class="post-entry-1">
+            <a href="single-post.html"><img src="assets/img/post-landscape-6.jpg" alt="" class="img-fluid"></a>
+            <div class="post-meta"><span class="date">Lifestyle</span> <span class="mx-1">&bullet;</span> <span>Jul 5th '22</span></div>
+            <h2><a href="single-post.html">Let’s Get Back to Work, New York</a></h2>
+          </div>
+          <div class="post-entry-1">
+            <a href="single-post.html"><img src="assets/img/post-landscape-5.jpg" alt="" class="img-fluid"></a>
+            <div class="post-meta"><span class="date">Lifestyle</span> <span class="mx-1">&bullet;</span> <span>Jul 17th '22</span></div>
+            <h2><a href="single-post.html">How to Avoid Distraction and Stay Focused During Video Calls?</a></h2>
+          </div>
+          <div class="post-entry-1">
+            <a href="single-post.html"><img src="assets/img/post-landscape-4.jpg" alt="" class="img-fluid"></a>
+            <div class="post-meta"><span class="date">Lifestyle</span> <span class="mx-1">&bullet;</span> <span>Mar 15th '22</span></div>
+            <h2><a href="single-post.html">Why Craigslist Tampa Is One of The Most Interesting Places On the Web?</a></h2>
+          </div>
+        </div>
+        <div class="col-lg-4 border-start custom-border">
+          <div class="post-entry-1">
+            <a href="single-post.html"><img src="assets/img/post-landscape-3.jpg" alt="" class="img-fluid"></a>
+            <div class="post-meta"><span class="date">Lifestyle</span> <span class="mx-1">&bullet;</span> <span>Jul 5th '22</span></div>
+            <h2><a href="single-post.html">6 Easy Steps To Create Your Own Cute Merch For Instagram</a></h2>
+          </div>
+          <div class="post-entry-1">
+            <a href="single-post.html"><img src="assets/img/post-landscape-2.jpg" alt="" class="img-fluid"></a>
+            <div class="post-meta"><span class="date">Lifestyle</span> <span class="mx-1">&bullet;</span> <span>Mar 1st '22</span></div>
+            <h2><a href="single-post.html">10 Life-Changing Hacks Every Working Mom Should Know</a></h2>
+          </div>
+          <div class="post-entry-1">
+            <a href="single-post.html"><img src="assets/img/post-landscape-1.jpg" alt="" class="img-fluid"></a>
+            <div class="post-meta"><span class="date">Lifestyle</span> <span class="mx-1">&bullet;</span> <span>Jul 5th '22</span></div>
+            <h2><a href="single-post.html">5 Great Startup Tips for Female Founders</a></h2>
+          </div>
+        </div>
+        <div class="col-lg-4">
+
+          <div class="post-entry-1 border-bottom">
+            <div class="post-meta"><span class="date">Lifestyle</span> <span class="mx-1">&bullet;</span> <span>Jul 5th '22</span></div>
+            <h2 class="mb-2"><a href="single-post.html">How to Avoid Distraction and Stay Focused During Video Calls?</a></h2>
+            <span class="author mb-3 d-block">Jenny Wilson</span>
+          </div>
+
+          <div class="post-entry-1 border-bottom">
+            <div class="post-meta"><span class="date">Lifestyle</span> <span class="mx-1">&bullet;</span> <span>Jul 5th '22</span></div>
+            <h2 class="mb-2"><a href="single-post.html">17 Pictures of Medium Length Hair in Layers That Will Inspire Your New Haircut</a></h2>
+            <span class="author mb-3 d-block">Jenny Wilson</span>
+          </div>
+
+          <div class="post-entry-1 border-bottom">
+            <div class="post-meta"><span class="date">Lifestyle</span> <span class="mx-1">&bullet;</span> <span>Jul 5th '22</span></div>
+            <h2 class="mb-2"><a href="single-post.html">9 Half-up/half-down Hairstyles for Long and Medium Hair</a></h2>
+            <span class="author mb-3 d-block">Jenny Wilson</span>
+          </div>
+
+          <div class="post-entry-1 border-bottom">
+            <div class="post-meta"><span class="date">Lifestyle</span> <span class="mx-1">&bullet;</span> <span>Jul 5th '22</span></div>
+            <h2 class="mb-2"><a href="single-post.html">Life Insurance And Pregnancy: A Working Mom’s Guide</a></h2>
+            <span class="author mb-3 d-block">Jenny Wilson</span>
+          </div>
+
+          <div class="post-entry-1 border-bottom">
+            <div class="post-meta"><span class="date">Lifestyle</span> <span class="mx-1">&bullet;</span> <span>Jul 5th '22</span></div>
+            <h2 class="mb-2"><a href="single-post.html">The Best Homemade Masks for Face (keep the Pimples Away)</a></h2>
+            <span class="author mb-3 d-block">Jenny Wilson</span>
+          </div>
+
+          <div class="post-entry-1 border-bottom">
+            <div class="post-meta"><span class="date">Lifestyle</span> <span class="mx-1">&bullet;</span> <span>Jul 5th '22</span></div>
+            <h2 class="mb-2"><a href="single-post.html">10 Life-Changing Hacks Every Working Mom Should Know</a></h2>
+            <span class="author mb-3 d-block">Jenny Wilson</span>
+          </div>
+
+        </div>
+      </div>
+    </div>
+
+  </div> <!-- End .row -->
+</div>
--- public/assets/js/loadHomeData.js
+++ public/assets/js/loadHomeData.js
@@ -4,42 +4,69 @@ async function loadHomeData(){
   var bannerHome = document.querySelector('._bannerHome');
   var recent = document.querySelector('._recent');
   var categoryCulture = document.querySelector('._category_culture');
+  var categoryBusiness = document.querySelector('._category_business');
+  var categoryLifeStyle = document.querySelector('._category_lifestyle');
 
-  const responses = await Promise.all([
-    await fetch('/banner/home', { method: 'get' }),
-    await fetch('/trendings', { method: 'get' }),
-    await fetch('/recent', { method: 'get' }),
-    await fetch('/category/culture', { method: 'get' }),
-    // await fetch('/category/business', { method: 'get' }),
-    // await fetch('/category/lifestyle', { method: 'get' }),
-  ]);
+  // const responses = await Promise.all([
+  //   await fetch('/banner/home', { method: 'get' }),
+  //   await fetch('/trendings', { method: 'get' }),
+  //   await fetch('/recent', { method: 'get' }),
+  //   await fetch('/category/culture', { method: 'get' }),
+  //   await fetch('/category/business', { method: 'get' }),
+  //   await fetch('/category/lifestyle', { method: 'get' }),
+  // ]);
 
-  const bannerHtml = await responses[0].text();
-  const trendingHtml = await responses[1].text();
-  const recentHtml = await responses[2].text();
-  const categoryCultureHtml = await responses[3].text();
+  // const bannerHtml = await responses[0].text();
+  // const trendingHtml = await responses[1].text();
+  // const recentHtml = await responses[2].text();
+  // const categoryCultureHtml = await responses[3].text();
+  // const categoryBusinessHtml = await responses[4].text();
+  // const categoryLifeStyleHtml = await responses[5].text();
 
-  bannerHome.innerHTML = bannerHtml;
-  recent.innerHTML = recentHtml;
-  
-  var trending = document.querySelector('._trending');
-  trending.innerHTML = trendingHtml;
-
-  categoryCulture.innerHTML = categoryCultureHtml;
-
-  // // !! load banner home
-  // const dataBanner = await fetch('/banner/home',{method:'get'});
-  // const bannerHtml = await dataBanner.text();
   // bannerHome.innerHTML = bannerHtml;
-
-  // // !! load trendings
-  // const dataTrending = await fetch('/trendings',{method:'get'});
-  // const trendingHtml = await dataTrending.text();
+  // recent.innerHTML = recentHtml;
+  
+  // var trending = document.querySelector('._trending');
   // trending.innerHTML = trendingHtml;
+
+  // categoryCulture.innerHTML = categoryCultureHtml;
+  // categoryBusiness.innerHTML = categoryBusinessHtml;
+  // categoryLifeStyle.innerHTML = categoryLifeStyleHtml;
+
+  // !! load banner home
+  const dataBanner = await fetch('/banner/home',{method:'get'});
+  const bannerHtml = await dataBanner.text();
+  bannerHome.innerHTML = bannerHtml;
 
   swipe('.sliderFeaturedPosts');
 
+  // !! load recent
+  const datataRecent = await fetch('/recent',{method:'get'});
+  const recentHtml = await datataRecent.text();
+  recent.innerHTML = recentHtml;
 
+  // !! load trendings
+  var trending = document.querySelector('._trending');
+  const dataTrending = await fetch('/trendings',{method:'get'});
+  const trendingHtml = await dataTrending.text();
+  trending.innerHTML = trendingHtml;
+
+  // !! Category culture
+  const dataCategoryCulture = await fetch('/category/culture',{method:'get'});
+  const categoryCultureHtml = await dataCategoryCulture.text();
+  categoryCulture.innerHTML = categoryCultureHtml;
+
+  
+  // !! Category business
+  const dataCategoryBusiness = await fetch('/category/business',{method:'get'});
+  const categoryBusinessHtml = await dataCategoryBusiness.text();
+  categoryBusiness.innerHTML = categoryBusinessHtml;
+
+  
+  // !! Category lifestyle
+  const dataCategoryLifeStyle = await fetch('/category/lifestyle',{method:'get'});
+  const categoryLifeStyleHtml = await dataCategoryLifeStyle.text();
+  categoryLifeStyle.innerHTML = categoryLifeStyleHtml;
 
 }
 
