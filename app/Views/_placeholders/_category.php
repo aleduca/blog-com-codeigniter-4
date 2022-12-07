8a066c8c5dd151fa3b483af556186781ed865683
Alexandre Cardoso <xandecar@hotmail.com>
2022-12-07 15:46:42 -0300

    Categories loading with placeholders

---
+++ app/Views/_placeholders/_category.php
@@ -0,0 +1,92 @@
+<div class="container _container" data-aos="fade-up">
+  <div class="section-header d-flex justify-content-between align-items-center mb-5">
+    <h2>Business</h2>
+    <div><a href="category.html" class="more">See All Business</a></div>
+  </div>
+
+  <div class="row">
+    <div class="col-md-9 order-md-2">
+
+      <div class="d-lg-flex post-entry-2">
+        <a href="single-post.html" class="me-4 thumbnail d-inline-block mb-4 mb-lg-0">
+          <!-- <img src="assets/img/post-landscape-3.jpg" alt="" class="img-fluid"> -->
+        </a>
+        <div>
+          <div class="post-meta"><span class="date">Business</span> <span class="mx-1">&bullet;</span> <span>Jul 5th '22</span></div>
+          <h3><a href="single-post.html">What is the son of Football Coach John Gruden, Deuce Gruden doing Now?</a></h3>
+          <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Distinctio placeat exercitationem magni voluptates dolore. Tenetur fugiat voluptates quas, nobis error deserunt aliquam temporibus sapiente, laudantium dolorum itaque libero eos deleniti?</p>
+          <div class="d-flex align-items-center author">
+            <!-- <div class="photo"><img src="assets/img/person-4.jpg" alt="" class="img-fluid"></div> -->
+            <div class="name">
+              <h3 class="m-0 p-0">Wade Warren</h3>
+            </div>
+          </div>
+        </div>
+      </div>
+
+      <div class="row">
+        <div class="col-lg-4">
+          <div class="post-entry-1 border-bottom">
+            <!-- <a href="single-post.html"><img src="assets/img/post-landscape-5.jpg" alt="" class="img-fluid"></a> -->
+            <div class="post-meta"><span class="date">Business</span> <span class="mx-1">&bullet;</span> <span>Jul 5th '22</span></div>
+            <h2 class="mb-2"><a href="single-post.html">11 Work From Home Part-Time Jobs You Can Do Now</a></h2>
+            <span class="author mb-3 d-block">Jenny Wilson</span>
+            <p class="mb-4 d-block">Lorem ipsum dolor sit, amet consectetur adipisicing elit. Vero temporibus repudiandae, inventore pariatur numquam cumque possimus</p>
+          </div>
+
+          <div class="post-entry-1">
+            <div class="post-meta"><span class="date">Business</span> <span class="mx-1">&bullet;</span> <span>Jul 5th '22</span></div>
+            <h2 class="mb-2"><a href="single-post.html">5 Great Startup Tips for Female Founders</a></h2>
+            <span class="author mb-3 d-block">Jenny Wilson</span>
+          </div>
+        </div>
+        <div class="col-lg-8">
+          <div class="post-entry-1">
+            <!-- <a href="single-post.html"><img src="assets/img/post-landscape-7.jpg" alt="" class="img-fluid"></a> -->
+            <div class="post-meta"><span class="date">Business</span> <span class="mx-1">&bullet;</span> <span>Jul 5th '22</span></div>
+            <h2 class="mb-2"><a href="single-post.html">How to Avoid Distraction and Stay Focused During Video Calls?</a></h2>
+            <span class="author mb-3 d-block">Jenny Wilson</span>
+            <p class="mb-4 d-block">Lorem ipsum dolor sit, amet consectetur adipisicing elit. Vero temporibus repudiandae, inventore pariatur numquam cumque possimus</p>
+          </div>
+        </div>
+      </div>
+    </div>
+    <div class="col-md-3">
+      <div class="post-entry-1 border-bottom">
+        <div class="post-meta"><span class="date">Business</span> <span class="mx-1">&bullet;</span> <span>Jul 5th '22</span></div>
+        <h2 class="mb-2"><a href="single-post.html">How to Avoid Distraction and Stay Focused During Video Calls?</a></h2>
+        <span class="author mb-3 d-block">Jenny Wilson</span>
+      </div>
+
+      <div class="post-entry-1 border-bottom">
+        <div class="post-meta"><span class="date">Business</span> <span class="mx-1">&bullet;</span> <span>Jul 5th '22</span></div>
+        <h2 class="mb-2"><a href="single-post.html">17 Pictures of Medium Length Hair in Layers That Will Inspire Your New Haircut</a></h2>
+        <span class="author mb-3 d-block">Jenny Wilson</span>
+      </div>
+
+      <div class="post-entry-1 border-bottom">
+        <div class="post-meta"><span class="date">Business</span> <span class="mx-1">&bullet;</span> <span>Jul 5th '22</span></div>
+        <h2 class="mb-2"><a href="single-post.html">9 Half-up/half-down Hairstyles for Long and Medium Hair</a></h2>
+        <span class="author mb-3 d-block">Jenny Wilson</span>
+      </div>
+
+      <div class="post-entry-1 border-bottom">
+        <div class="post-meta"><span class="date">Business</span> <span class="mx-1">&bullet;</span> <span>Jul 5th '22</span></div>
+        <h2 class="mb-2"><a href="single-post.html">Life Insurance And Pregnancy: A Working Mom’s Guide</a></h2>
+        <span class="author mb-3 d-block">Jenny Wilson</span>
+      </div>
+
+      <div class="post-entry-1 border-bottom">
+        <div class="post-meta"><span class="date">Business</span> <span class="mx-1">&bullet;</span> <span>Jul 5th '22</span></div>
+        <h2 class="mb-2"><a href="single-post.html">The Best Homemade Masks for Face (keep the Pimples Away)</a></h2>
+        <span class="author mb-3 d-block">Jenny Wilson</span>
+      </div>
+
+      <div class="post-entry-1 border-bottom">
+        <div class="post-meta"><span class="date">Business</span> <span class="mx-1">&bullet;</span> <span>Jul 5th '22</span></div>
+        <h2 class="mb-2"><a href="single-post.html">10 Life-Changing Hacks Every Working Mom Should Know</a></h2>
+        <span class="author mb-3 d-block">Jenny Wilson</span>
+      </div>
+    </div>
+  </div>
+</div>
--- app/Views/home.php
+++ app/Views/home.php
@@ -1,10 +1,8 @@
 <?= $this->extend('master') ?>
 
-
 <?= $this->section('css') ?>
 <link rel="stylesheet" href="<?php echo base_url('assets/css/fragment.css'); ?>">
 <?= $this->endSection() ?>
-
 
 <?= $this->section('content') ?>
     <!-- ======= Hero Slider Section ======= -->
@@ -21,14 +19,23 @@
 
     <!-- ======= Culture Category Section ======= -->
     <section class="category-section _category_culture">
+      <include-fragment src="/category/culture">
+          <?php echo $this->include('_placeholders/_category'); ?>
+      </include-fragment>
     </section><!-- End Culture Category Section -->
 
     <!-- ======= Business Category Section ======= -->
     <section class="category-section _category_business">
+        <include-fragment src="/category/business">
+          <?php echo $this->include('_placeholders/_category'); ?>
+      </include-fragment>
     </section><!-- End Business Category Section -->
 
     <!-- ======= Lifestyle Category Section ======= -->
     <section class="category-section _category_lifestyle">
+        <include-fragment src="/category/lifestyle">
+          <?php echo $this->include('_placeholders/_category'); ?>
+      </include-fragment>
     </section><!-- End Lifestyle Category Section -->
 <?= $this->endSection() ?>
 
