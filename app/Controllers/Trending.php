909e9da9d2668ff36080cb47c4f5e4202aee46f9
Alexandre Cardoso <xandecar@hotmail.com>
2022-12-30 13:49:46 -0300

    Lista dos trendings

--- app/Controllers/Trending.php
+++ app/Controllers/Trending.php
@@ -2,10 +2,23 @@
 
 namespace App\Controllers;
 
+use App\Models\Post;
+
 class Trending extends BaseController
 {
-    public function index()
-    {
-        return view('_partials/_trending');
-    }
+  public function index()
+  {
+    $post = new Post();
+    $posts = $post->select('
+      posts.title, users.firstName as userFirstName, users.lastName as userLastName
+    ')->orderBy(
+      'visits',
+      'desc'
+    )->join(
+      'users',
+      'users.id = posts.user_id'
+    )->findAll(5);
+
+    return view('_partials/_trending', ['posts' => $posts]);
+  }
 }
--- app/Views/_partials/_trending.php
+++ app/Views/_partials/_trending.php
@@ -1,38 +1,14 @@
  <h3>Trending</h3>
-  <ul class="trending-post">
-    <li>
-      <a href="single-post.html">
-        <span class="number">1</span>
-        <h3>The Best Homemade Masks for Face (keep the Pimples Away)</h3>
-        <span class="author">Jane Cooper</span>
-      </a>
-    </li>
-    <li>
-      <a href="single-post.html">
-        <span class="number">2</span>
-        <h3>17 Pictures of Medium Length Hair in Layers That Will Inspire Your New Haircut</h3>
-        <span class="author">Wade Warren</span>
-      </a>
-    </li>
-    <li>
-      <a href="single-post.html">
-        <span class="number">3</span>
-        <h3>13 Amazing Poems from Shel Silverstein with Valuable Life Lessons</h3>
-        <span class="author">Esther Howard</span>
-      </a>
-    </li>
-    <li>
-      <a href="single-post.html">
-        <span class="number">4</span>
-        <h3>9 Half-up/half-down Hairstyles for Long and Medium Hair</h3>
-        <span class="author">Cameron Williamson</span>
-      </a>
-    </li>
-    <li>
-      <a href="single-post.html">
-        <span class="number">5</span>
-        <h3>Life Insurance And Pregnancy: A Working Mom’s Guide</h3>
-        <span class="author">Jenny Wilson</span>
-      </a>
-    </li>
-  </ul>
+ <ul class="trending-post">
+   <?php foreach ($posts as $index => $post) : ?>
+     <li>
+       <a href="single-post.html">
+         <span class="number"><?php echo $index + 1 ?></span>
+         <h3><?php echo $post->title; ?></h3>
+         <span class="author">
+           <?php echo $post->userFirstName ?> <?php echo $post->userLastName ?>
+         </span>
+       </a>
+     </li>
+   <?php endforeach; ?>
+ </ul>
